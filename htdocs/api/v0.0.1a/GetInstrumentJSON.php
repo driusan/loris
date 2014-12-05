ed705280afba6374cb21d7279e501af0baf37376
Dave MacFarlane <driusan@gmail.com>
2014-12-05 10:00:57 -0500

    Added project visits to API

--- htdocs/api/v0.0.1a/GetInstrumentJSON.php
+++ htdocs/api/v0.0.1a/GetInstrumentJSON.php
@@ -5,9 +5,11 @@ get_include_path() . ":" .
 __DIR__ . "../../../php/libraries"
 );
 
+ header("Access-Control-Allow-Origin: *");
 // Ensures the user is logged in, and parses the config file.
 require_once "NDB_Client.class.inc";
 $client = new NDB_Client();
+$client->makeCommandLine();
 $client->initialize("../../../project/config.xml");
 
 
