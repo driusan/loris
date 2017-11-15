70858eb498e45e7f1b7917e640848d4e3aafa142
Dave MacFarlane <driusan@gmail.com>
2017-11-15 13:24:19 -0500

    Use Zend\Diactoros instead of homegrown PSR7 implementation

--- htdocs/uri.php
+++ htdocs/uri.php
@@ -5,44 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $client = new \NDB_Client;
 $client->initialize();
 
-$get = $_GET;
-$request = \LORIS\Http\ServerRequest::fromGlobals();
-
-// There are 2 entry-points to this file.
-// 1. A mod_rewrite rule that adds a lorispath query param.
-// 2. A ErrorDocument 404 handler that falls back to this script
-//    if Apache doesn't find anything.
-// They have different ways to get the "correct" page path from the URL
-// that the user was trying to access.
-if (isset($_GET['lorispath'])) {
-    // It's being done from mod_rewrite.
-    $url = \LORIS\Http\URI::fromGlobals()->withPath($_GET['lorispath']);
-    $paths = explode("/", $get['lorispath']);
-    unset($get['lorispath']);
-} else {
-    // It's being used as a 404 handler.
-    $url = \LORIS\Http\URI::fromGlobals()->withPath($_SERVER['REDIRECT_URL']);
-    $paths = explode("/", $url->getPath());
-    //parse_str($_SERVER['REDIRECT_QUERY_STRING'] ?? '', $get);
-}
-
-// Lexically resolve path components that have . or .. in them.
-// FIXME: This should be in a class where it's useable elsewhere in LORIS.
-$newpath = [];
-foreach($paths as $val) {
-    if ($val == ".") {
-        continue;
-    } else if ($val == "..") {
-        array_pop($newpath);
-    } else {
-        array_push($newpath, $val);
-    }
-}
-$url = $url->withPath(join("/", $newpath));
-// FIXME: This is broken.. it's missing the = part of the query.
-$url = $url->withQuery(join("&", $get));
-$request = $request->withURI($url)->withQueryParams($get);
-
+$request = \Zend\Diactoros\ServerRequestFactory::fromGlobals();
 // Now that we've created the ServerRequest, handle it.
 $loris = new \LORIS\Router\BaseRouter(__DIR__ . "/../project/", __DIR__ . "/../modules/");
 
