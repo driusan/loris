4b230b670219a6c13e539fa9553dd40469caea0a
Dave MacFarlane <driusan@gmail.com>
2020-08-31 14:41:02 -0400

    Convert instrument module to queryengine

--- src/Data/Query/Criteria/LessThanOrEqual.php
+++ src/Data/Query/Criteria/LessThanOrEqual.php
@@ -2,7 +2,7 @@
 
 namespace LORIS\Data\Query\Criteria;
 
-class LessThanOrEqual {
+class LessThanOrEqual  implements \LORIS\Data\Query\Criteria {
     public function __construct($val) {
         $this->val = $val;
     }
