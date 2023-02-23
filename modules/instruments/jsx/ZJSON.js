5590e57f578a179ea8fd8af97b771437fe942d92
Dave MacFarlane <driusan@mcin.ca>
2023-02-23 12:12:44 -0500

    add datetime element types

--- modules/instruments/jsx/ZJSON.js
+++ modules/instruments/jsx/ZJSON.js
@@ -56,7 +56,7 @@ function ZJSONPage(props) {
                     // XXX: Allow other data types? The spec doesn't say
                     // how they should be handled.
                     throw new Error(
-                        'Incompatible element and UI. select UI elements'
+                        'Incompatible element and UI. checkbox UI elements'
                         + ' must be backed by a boolean data type.'
                     );
                 }
@@ -66,6 +66,58 @@ function ZJSONPage(props) {
                             name={name}
                             label={label}
                         />;
+            case 'date':
+                if (elementSchema.type != 'date') {
+                    // XXX: Allow other data types? The spec doesn't say
+                    // how they should be handled.
+                    throw new Error(
+                        'Incompatible element and UI. date UI elements'
+                        + ' must be backed by a date data type.'
+                    );
+                }
+                // FIXME: Support all valid ZJSON options here.
+                return <DateElement
+                            key={idx}
+                            name={name}
+                            label={label}
+                        />;
+            case 'time':
+                if (elementSchema.type != 'time') {
+                    // XXX: Allow other data types? The spec doesn't say
+                    // how they should be handled.
+                    throw new Error(
+                        'Incompatible element and UI. time UI elements'
+                        + ' must be backed by a time data type.'
+                    );
+                }
+                // FIXME: Support all valid ZJSON options here.
+                return <TimeElement
+                            key={idx}
+                            name={name}
+                            label={label}
+                        />;
+            case 'datetime':
+                if (elementSchema.type != 'datetime') {
+                    // XXX: Allow other data types? The spec doesn't say
+                    // how they should be handled.
+                    throw new Error(
+                        'Incompatible element and UI. datetime UI elements'
+                        + ' must be backed by a datetime data type.'
+                    );
+                }
+                // FIXME: Support all valid ZJSON options here.
+                return <div>
+                            <DateElement
+                                key={idx + 'date'}
+                                name={name}
+                                label={label}
+                            />
+                            <TimeElement
+                                key={idx + 'time'}
+                                name={name}
+                                label={label}
+                            />
+                        </div>;
             default:
                 console.error('Unhandled element type: ' + elementUI.type);
             }
@@ -86,10 +138,15 @@ function ZJSONPage(props) {
             return obj.description[defaultLang];
     });
     const pageList = <ul>
-        {pageNames.map( (name, idx) => <li key={idx}>
+        {pageNames.map( (name, idx) => {
+            let style={};
+            if (idx == pageNum) {
+                style.fontWeight = 'bolder';
+            }
+            return <li key={idx} style={style}>
                 <a href="#" onClick={() => setPageNum(idx)}>{name}</a>
-            </li>
-        ) }
+            </li>;
+        })}
     </ul>;
 
     return <div><h1>{instrumentTitle}({defaultLang})</h1>
