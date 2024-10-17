dd1789555b78ea2afccdde2abed848451b2e8be9
Dave MacFarlane <dave.macfarlane@mcin.ca>
2024-10-17 13:49:19 -0400

    Store data

--- modules/neurohub/jsx/widgets.tsx
+++ modules/neurohub/jsx/widgets.tsx
@@ -1,9 +1,4 @@
-/**
- * Callback for clicking the NeuroHub button in the dataquery widget
- */
-export function NeurohubDataqueryCallback() {
-    // console.log('Clicked!');
-}
+import {useState, useEffect} from 'react'
 
 /**
  * Implements a User Preference widget for the Neurohub module
@@ -11,8 +6,20 @@ export function NeurohubDataqueryCallback() {
  * @returns {React.ReactElement} - The widget
  */
 export function NeurohubTokenPreference() {
-     return (<form>
-         <label htmlFor="neurohubtoken">Neurohub API token:</label>
-         <input type="text" id="neurohubtoken" />
-     </form>);
+     const [token, setToken] = useState(null);
+     useEffect( () => {
+	     fetch('/neurohub/token')
+	     .then( (response) => response.json())
+	     .then( (value) => {
+		     setToken(value.token);
+	     });
+     }, []);
+     return (
+        <div className="row form-group">
+          <label className="col-sm-2">Neurohub API token:</label>
+          <div className="col-sm-10">
+              <input name="neurohubtoken" className="form-control input-sm" type="text" id="neurohubtoken" />
+          </div>
+        </div>
+     );
 }
